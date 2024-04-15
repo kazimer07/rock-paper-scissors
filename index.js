d84603a311265cc8d12bf643cf7d41e60abb176e
Kazimer Szymanski <kazimer07@proton.me>
2024-04-14 21:05:52 -0400

    getComputerChoice completed

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 function playRound(playerSelection, computerSelection) {
-    
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
+            
+        }
+    }
 }
 
 function getComputerChoice() {
+    choice = Math.floor(Math.random() * 3);
     
+    if (choice === 0) {
+        return "rock";
+    } else if (choice == 1){
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
+
   
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
