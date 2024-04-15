0ff7f6efe4c77b6d23f6dbfd1c84ad274bf1d186
Kazimer Szymanski <kazimer07@proton.me>
2024-04-14 21:27:39 -0400

    Working game

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
+function win() {
+    console.log("You win! " + playerSelection + " beats " + computerSelection + ".");
+}
+
+function lose() {
+    console.log("You lose! "  + computerSelection + " beats " + playerSelection + ".");
+}
+
+function tie() {
+    console.log("It's a tie! You both chose " + playerSelection + ".");
+}
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
-            
+            tie();
+        } else if (computerSelection === "scissors") {
+            win();
+        } else {
+            lose();
         }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "paper") {
+            tie();
+        } else if (computerSelection === "rock") {
+            win();
+        } else {
+            lose();
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "scissors") {
+            tie();
+        } else if (computerSelection === "paper") {
+            win();
+        } else {
+            lose();
+        }
+    } else {
+        console.log("Enter rock, paper, or scissors only!");
     }
 }
 
@@ -19,7 +53,8 @@ function getComputerChoice() {
 }
 
   
-const playerSelection = "rock";
+let playerSelection = prompt("Choose rock, paper, or scissors!");
+playerSelection = playerSelection.toLowerCase();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
   
