134ddff53340841fd45da56c805a9328c599e1f4
Kazimer Szymanski <kazimer07@proton.me>
2024-04-14 20:51:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function playRound(playerSelection, computerSelection) {
+    
+}
+
+function getComputerChoice() {
+    
+}
+  
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+  
